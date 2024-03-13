999041dc35ed4bc95e439ed11829fb4d703185d4
Brighter <brighteragyemang@gmail.com>
2024-03-13 12:35:34 +1100

    added setup file to allow editable install

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='NIQL',
+    version='0.1',
+    packages=['niql', 'niql.algo', 'niql.scripts', 'train'],
+    url='',
+    license='MIT',
+    author='brighteragyemang',
+    author_email='brighteragyemang@gmail.com',
+    description=''
+)
