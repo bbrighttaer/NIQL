a58efbd5fe6083af0edc08584a40fd670fb88f27
Brighter <brighteragyemang@gmail.com>
2024-03-20 14:00:18 +1100

    set core arch via terminal

--- train/iql.py
+++ train/iql.py
@@ -1,8 +1,22 @@
+from argparse import ArgumentParser
+
 import torch
 from marllib import marl
 from niql import env, scripts, config
 
 if __name__ == '__main__':
+    parser = ArgumentParser()
+
+    parser.add_argument(
+        '-m', '--model_arch',
+        default='gru',
+        type=str,
+        choices=['mlp', 'gru', 'lstm'],
+        help='The core architecture of the model',
+    )
+
+    args = parser.parse_args()
+
     # get env
     env = env.make_mpe_env()
 
@@ -14,7 +28,9 @@ if __name__ == '__main__':
     iql.algo_parameters = config.mpe['algo_parameters']
 
     # build agent model based on env + algorithms + user preference if checked available
-    model = marl.build_model(env, iql, model_preference=config.mpe['model_preference'])
+    model_config = config.mpe['model_preference']
+    model_config.update({'core_arch': args.model_arch})
+    model = marl.build_model(env, iql, model_preference=model_config)
 
     # start learning + extra experiment settings if needed. remember to check ray.yaml before use
     gpu_count = torch.cuda.device_count()
