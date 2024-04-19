imix and neighbour gradients bug fix

niql/algo/imix.py
@@ -1,6 +1,6 @@
 import functools
 import logging
-from typing import Union, List, Optional, Dict, Tuple
+from typing import Union, List, Optional, Dict, Tuple, Any
 
 import numpy as np
 import torch
@@ -20,6 +20,7 @@ from ray.rllib.utils.torch_ops import convert_to_torch_tensor, convert_to_non_to
 from ray.rllib.utils.torch_ops import huber_loss
 from ray.rllib.utils.typing import TensorStructType, TensorType, AgentID
 
+from niql.comm import CommMsg
 from niql.config import FINGERPRINT_SIZE
 
 logger = logging.getLogger(__name__)
@@ -176,6 +177,9 @@ class IMIX(Policy):
         if self.model:
             self.model.eval()
 
+        if self.mixer:
+            self.mixer.eval()
+
         with torch.no_grad():
             seq_lens = torch.ones(len(obs_batch), dtype=torch.int32)
             obs_batch = SampleBatch({
@@ -228,6 +232,9 @@ class IMIX(Policy):
         # Set Model to train mode.
         if self.model:
             self.model.train()
+
+        if self.mixer:
+            self.mixer.train()
 
         # Callback handling.
         learn_stats = {}
@@ -370,7 +377,7 @@ class IMIX(Policy):
 
                 n_qt_prime, _ = n_policy.model(next_obs, state_batches_h, seq_lens)
                 n_qt_prime_best_one_hot_selection = F.one_hot(torch.argmax(n_qt_prime, 1), self.action_space.n)
-                n_qt_prime_best = torch.sum(n_qt_prime * n_qt_prime_best_one_hot_selection, dim=1, keepdim=True)
+                n_qt_prime_best = torch.sum(n_qt_prime.detach() * n_qt_prime_best_one_hot_selection, dim=1, keepdim=True)
                 n_qt_prime_best_masked = (1.0 - dones.view(-1, 1)) * n_qt_prime_best
                 all_qt_prime_best.append(n_qt_prime_best_masked)
 
@@ -456,3 +463,50 @@ class IMIX(Policy):
                 new_wts += n_weights[n_id] * n_mixer_wts[layer_i]
             weights[layer_i] = new_wts
         self.mixer.load_state_dict(weights)
+
+    def compute_eval_actions(
+            self,
+            obs_batch,
+            state_batches,
+            n_obs,
+            n_policy,
+    ) -> Dict[str, Any]:
+        """
+        Method for analysing matrix game.
+
+        :param obs_batch:
+        :param state_batches:
+        :return:
+        """
+        assert self.mixer is not None, "mixer is cannot be null"
+
+        # switch to eval mode.
+        self.model.eval()
+        self.mixer.eval()
+
+        seq_lens = torch.ones(len(obs_batch), dtype=torch.int32)
+        state_batches = [
+            convert_to_torch_tensor(s, self.device) for s in (state_batches or [])
+        ]
+        # batch preprocessing ops
+        obs_batch = self.convert_batch_to_tensor({
+            SampleBatch.OBS: obs_batch
+        })
+        obs_batch.set_get_interceptor(
+            functools.partial(convert_to_torch_tensor, device=self.device)
+        )
+        n_obs = convert_to_torch_tensor(n_obs, device=self.device)
+
+        # predict q-vals
+        q_vals_out, _ = self.model(obs_batch, state_batches, seq_lens)
+        n_q_vals_out, _ = n_policy.model(obs_batch, state_batches, seq_lens)
+
+        # nonlinear mixing
+        tensor = torch.tensor([q_vals_out.squeeze()[0], n_q_vals_out.squeeze()[0]]).view(1, -1)
+        state = torch.cat([obs_batch[SampleBatch.OBS], n_obs], dim=1).float()
+        q_tot = self.mixer(tensor, state).squeeze()
+
+        return {
+            "q_values": q_vals_out.squeeze().detach().numpy().tolist(),
+            "q_tot": q_tot.squeeze().item()
+        }

niql/utils.py
@@ -19,7 +19,7 @@ from ray.rllib.models.torch.torch_action_dist import TorchCategorical
 from ray.tune import register_env
 from ray.util.ml_utils.dict import merge_dicts
 
-from niql.algo import IQLTrainer
+from niql.algo import IQLTrainer, IMIXTrainer
 from niql.envs.wrappers import create_fingerprint_env_wrapper_class
 
 
@@ -174,23 +174,13 @@ def load_iql_checkpoint(model_class, exp, run_config, env, stop, restore) -> Che
             "count_steps_by": "env_steps",
         })
 
-    mixer_dict = {
-        "qmix": "qmix",
-        "vdn": "vdn",
-        "iql": None
-    }
-
     back_up_config["num_agents"] = 1  # one agent one model IQL
-    config = {
-        "model": {
-            "max_seq_len": episode_limit,  # dynamic
-            "custom_model_config": back_up_config,
-            "custom_model": model_name,
-            "lstm_cell_size": None,
-        },
+    model_config = {
+        "max_seq_len": episode_limit,  # dynamic
+        "custom_model_config": back_up_config,
+        "custom_model": model_name,
+        "lstm_cell_size": None,
     }
-
-    config.update(run_config)
 
     IQL_Config.update(
         {
@@ -206,7 +196,8 @@ def load_iql_checkpoint(model_class, exp, run_config, env, stop, restore) -> Che
                 "final_epsilon": final_epsilon,
                 "epsilon_timesteps": epsilon_timesteps,
             },
-            "mixer": mixer_dict[algorithm]
+            "mixer": exp["algo_args"]["mixer"] if algorithm == 'imix' else algorithm,
+            "max_neighbours": env["num_agents"] - 1,
         })
 
     IQL_Config["reward_standardize"] = reward_standardize  # this may affect the final performance if you turn it on
@@ -224,13 +215,20 @@ def load_iql_checkpoint(model_class, exp, run_config, env, stop, restore) -> Che
         IQL_Config["gamma"] = _param["gamma"]
 
     # create trainer
-    IQL_Config.update(config)
+    IQL_Config.update(run_config)
+    IQL_Config["model"].update(model_config)
     model_path = restore_model(restore, exp)
-    trainer_class = IQLTrainer.with_updates(
-        name=algorithm.upper(),
-        default_config=IQL_Config,
-    )
-    trainer = trainer_class(logger_creator=lambda c: NullLogger(c))
+    if algorithm == 'imix':
+        trainer_class = IMIXTrainer.with_updates(
+            name=algorithm.upper(),
+            default_config=IQL_Config,
+        )
+    else:
+        trainer_class = IQLTrainer.with_updates(
+            name=algorithm.upper(),
+            default_config=IQL_Config,
+        )
+    trainer = trainer_class(config=IQL_Config, logger_creator=lambda c: NullLogger(c))
     trainer.restore(model_path)
 
     map_name = exp["env_args"]["map_name"]

train/imix.py
@@ -68,7 +68,7 @@ if __name__ == '__main__':
     marl.algos.register_algo(
         algo_name="imix",
         style="il",
-        script=scripts.run_imix  # if mode == 'train' else utils.load_iql_checkpoint,
+        script=scripts.run_imix if mode == 'train' else utils.load_iql_checkpoint,
     )
 
     # initialize algorithm
@@ -98,18 +98,11 @@ if __name__ == '__main__':
             use_fingerprint=args.use_fingerprint,
         )
     else:
-        base = 'exp_results/iql_mlp_all_scenario/IQL_CoopMatrixGame_all_scenario_c383e_00000_0_2024-04-11_18-30-33'
+        base = 'exp_results/imix_mlp_all_scenario/IMIX_CoopMatrixGame_all_scenario_101a9_00000_0_2024-04-19_16-00-02'
         restore_path = {
             'params_path': f'{base}/params.json',  # experiment configuration
             'model_path': f'{base}/checkpoint_000010/checkpoint-10',  # checkpoint path
         }
-
-        # register new algorithm
-        marl.algos.register_algo(
-            algo_name="iql",
-            style="il",
-            script=scripts.run_iql if mode == 'train' else utils.load_iql_checkpoint,
-        )
 
         results = imix.fit(
             env,
@@ -137,24 +130,28 @@ if __name__ == '__main__':
                 actor_id: agent.get_policy(pmap(actor_id)).get_initial_state()
                 for actor_id in obs
             }
+            neighbour_lookup = {}
+            for actor_id in obs:
+                for n_id in obs:
+                    if actor_id != n_id:
+                        neighbour_lookup[actor_id] = n_id
 
             step = 0
             with torch.no_grad():
-                while not done["__all__"]:
-                    action_dict = {}
-                    cur_state = [0, 0, 1]
-                    for agent_id in obs.keys():
-                        policy = agent.get_policy(pmap(agent_id))
-                        agent_obs = [cur_state]  # obs[agent_id]["obs"]
-                        action_dict[agent_id], states[agent_id], info = policy.compute_single_action(
-                            agent_obs,
-                            states[agent_id],
-                            explore=False,
-                        )
-                        print(f'state={step}, agent={agent_id}, q-values={info["q-values"]}')
+                action_dict = {}
+                cur_state = np.array([[0, 0, 1]])
+                for agent_id in obs.keys():
+                    policy = agent.get_policy(pmap(agent_id))
+                    n_policy = agent.get_policy(pmap(neighbour_lookup[agent_id]))
+                    info = policy.compute_eval_actions(
+                        cur_state,
+                        states[agent_id],
+                        cur_state,
+                        n_policy,
+                    )
+                    print(f'state={step}, agent={agent_id}, q-values={info["q_values"]}, q_tot: {info["q_tot"]}')
 
-                    obs, reward, done, info = env_instance.step(action_dict)
-                    step += 1
+                step += 1
 
         env_instance.close()
         ray.shutdown()