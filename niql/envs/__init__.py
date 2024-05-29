11fe8023b8bda5a029d79c51d6072bb4f67e54db
Brighter <brighteragyemang@gmail.com>
2024-05-30 01:16:12 +1000

    set active env to pp

--- niql/envs/__init__.py
+++ niql/envs/__init__.py
@@ -14,7 +14,7 @@ DEBUG_ENVS = ["TwoStepsCoopMatrixGame", "OneStepCoopMatrixGame"]
 
 
 def get_active_env(**kwargs):
-    return make_two_step_matrix_game_env(**kwargs)
+    return make_predator_prey_env(**kwargs)
 
 
 def make_mpe_simple_spread_env(**kwargs):
