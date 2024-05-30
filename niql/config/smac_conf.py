7c5263bb5ba7c0addd80265d38a0510ca4001eb1
Brighter <brighteragyemang@gmail.com>
2024-05-30 18:18:10 +1000

    pp config update

--- niql/config/smac_conf.py
+++ niql/config/smac_conf.py
@@ -27,7 +27,7 @@ SMAC = {
     'model_preference': {
         'core_arch': 'gru',  # mlp | gru
         "encode_layer": "64",  # for RNN model
-        'hidden_state_size': 128,  # for RNN model
+        'hidden_state_size': 64,  # for RNN model
         'fcnet_activation': 'relu',
         'model': 'DRQNModel',
         # 'model': 'DuelingQFCN',
