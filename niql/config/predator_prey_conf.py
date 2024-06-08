3a1480e858047d5e14b01a322d64d59af3e35c31
Brighter <brighteragyemang@gmail.com>
2024-06-09 00:36:41 +1000

    set enc and hdmi to 128

--- niql/config/predator_prey_conf.py
+++ niql/config/predator_prey_conf.py
@@ -30,8 +30,8 @@ PREDATOR_PREY = {
     },
     'model_preference': {
         'core_arch': 'gru',  # mlp | gru
-        "encode_layer": "64",  # for RNN model
-        'hidden_state_size': 64,  # for RNN model
+        "encode_layer": "128",  # for RNN model
+        'hidden_state_size': 128,  # for RNN model
         'fcnet_activation': 'relu',
         'model': 'DRQNModel',
         # 'model': 'DuelingQFCN',
