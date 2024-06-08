bde2ab9b41e4d9e745f2f0cc00588f770409c41f
Brighter <brighteragyemang@gmail.com>
2024-06-08 22:37:19 +1000

    enable stochastic eviction per in pp config

--- niql/config/predator_prey_conf.py
+++ niql/config/predator_prey_conf.py
@@ -12,7 +12,7 @@ PREDATOR_PREY = {
             # ],
             'rollout_fragment_length': 1,
             'buffer_size': 5000,
-            'enable_stochastic_eviction': False,
+            'enable_stochastic_eviction': True,
             'target_network_update_freq': 200,
             'final_epsilon': 0.05,
             'epsilon_timesteps': 50000,
