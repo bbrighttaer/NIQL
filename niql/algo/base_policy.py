tdw learning with corrected weighting obj exp check

niql/algo/base_policy.py
@@ -116,7 +116,7 @@ class NIQLBasePolicy(LearningRateSchedule, Policy, ABC):
         models_factory_class.__init__(self, agent_obs_space, action_space, config, core_arch)
         vae_args = model_arch_args = config["model"]["custom_model_config"]["model_arch_args"]["tdw_vae"]
         self.vae_model = VAE(
-            input_dim=1,
+            input_dim=self.obs_size + self.n_actions + 1,
             hidden_layer_dims=vae_args["hdims"],
             latent_dim=vae_args["latent_dim"],
         ).to(self.device)
@@ -446,10 +446,16 @@ class NIQLBasePolicy(LearningRateSchedule, Policy, ABC):
         KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
         return MSE + KLD
 
-    def fit_vae(self, training_data, num_epochs=1):
-        # input_data = self.construct_tdw_dataset(training_data)
+    def compute_vae_loss(self, data):
+        vae = self.model.encoder
+        outputs, mu, logvar = vae.encode_decode(data)
+        loss = self.vae_loss_function(outputs, data, mu, logvar)
+        return loss
 
-        dataset = TensorDataset(training_data)
+    def fit_vae(self, training_data, num_epochs=5):
+        input_data = self.construct_tdw_dataset(training_data)
+
+        dataset = TensorDataset(input_data)
         data_loader = DataLoader(dataset, batch_size=32, shuffle=True)
 
         self.vae_model.train()
@@ -480,18 +486,34 @@ class NIQLBasePolicy(LearningRateSchedule, Policy, ABC):
         data = torch.cat([obs, actions, rewards], dim=-1)
         return data
 
-    def get_tdw_weights(self, targets):
-        targets_flat = standardize(targets.view(-1, 1))
-        if random.random() < self.tdw_schedule.value(self.global_timestep):
-            self.fit_vae(targets_flat)
-            densities = self.vae_model.estimate_density(targets_flat)
-            densities += 1e-7
+    # def get_tdw_weights(self, data):
+    #     if self.global_timestep > 10000 and random.random() < self.tdw_schedule.value(self.global_timestep):
+    #         vae = self.model.encoder
+    #         densities = vae.estimate_density(data)
+    #         tdw_weights = 1. / (densities + 1e-7)
+    #         tdw_weights /= tdw_weights.max()
+    #         # min_w = max(1e-2, tdw_weights.min())
+    #         # tdw_weights = torch.clamp(torch.log(tdw_weights), min_w, max=2 * min_w)
+    #
+    #         tb_add_scalars(self, "tdw_stats", {
+    #             # "scaling": scaling,
+    #             "max_weight": tdw_weights.max(),
+    #             "min_weight": tdw_weights.min(),
+    #             "mean_weight": tdw_weights.mean(),
+    #         })
+    #     else:
+    #         tdw_weights = torch.ones((len(data), 1))
+    #     return tdw_weights
 
+    def get_tdw_weights(self, training_data, obs, actions, rewards):
+        if training_data and random.random() < self.tdw_schedule.value(self.global_timestep):
+            self.fit_vae(training_data)
+
+            actions = torch.eye(self.n_actions, self.n_actions).to(self.device).float()[actions.view(-1, )]
+            data = torch.cat([obs, actions, rewards], dim=-1)
+            densities = self.vae_model.estimate_density(data)
             tdw_weights = 1. / (densities + 1e-7)
             tdw_weights /= (tdw_weights.max() + 1e-7)
-
-            # min_w = max(1e-10, tdw_weights.min())
-            # tdw_weights = torch.clamp(torch.log(1. + tdw_weights), max=5)
 
             tb_add_scalars(self, "tdw_stats", {
                 # "scaling": scaling,
@@ -500,7 +522,7 @@ class NIQLBasePolicy(LearningRateSchedule, Policy, ABC):
                 "mean_weight": tdw_weights.mean(),
             })
         else:
-            tdw_weights = torch.ones_like(targets)
+            tdw_weights = torch.ones_like(rewards)
         return tdw_weights
 
     def adaptive_gamma(self, alpha=0.01, beta=10000):

niql/algo/wiql.py
@@ -3,6 +3,7 @@ from functools import partial
 
 import numpy as np
 import torch
+from ray.rllib import SampleBatch
 from ray.rllib.utils.torch_ops import huber_loss
 
 from niql.algo.base_policy import NIQLBasePolicy
@@ -11,6 +12,13 @@ from niql.utils import to_numpy, tb_add_scalar, \
     target_distribution_weighting, unroll_mac, unroll_mac_squeeze_wrapper, soft_update, tb_add_histogram
 
 logger = logging.getLogger(__name__)
+
+
+def construct_vae_data(obs, prev_actions=None, n_actions=None):
+    if prev_actions is not None and n_actions:
+        actions_enc = torch.eye(n_actions).float().to(obs.device)[prev_actions.view(-1, )]
+        obs = torch.cat([obs, actions_enc], dim=-1)
+    return obs
 
 
 class WIQL(NIQLBasePolicy):
@@ -134,12 +142,24 @@ class WIQL(NIQLBasePolicy):
         #     SampleBatch.ACTIONS: actions.view(B * T, -1),
         #     SampleBatch.REWARDS: rewards.view(B * T, -1),
         # }))
-        tdw_weights = target_distribution_weighting(
-            self, targets.detach().clone().view(B * T, -1), self.config["similarity_threshold"]
-        )
-        tdw_weights = tdw_weights.view(B, T)
+        # tdw_weights = target_distribution_weighting(
+        #     self, targets.detach().clone().view(B * T, -1), self.config["similarity_threshold"]
+        # )
+        # tdw_weights = tdw_weights.view(B, T)
         # tdw_weights = self.get_tdw_weights(targets)
         # tdw_weights = tdw_weights.view(*targets.shape)
+        # vae_data = construct_vae_data(obs.view(B * T, -1), prev_actions, kwargs.get("n_actions"))
+        # tdw_weights = self.get_tdw_weights(
+        #     data=vae_data
+        # )
+        # tdw_weights = tdw_weights.view(*targets.shape)
+        tdw_weights = self.get_tdw_weights(
+            training_data=uniform_batch,
+            obs=obs.view(B * T, -1),
+            actions=actions.view(B * T, -1),
+            rewards=rewards.view(B * T, -1),
+        )
+        tdw_weights = tdw_weights.view(*targets.shape)
 
         # Td-error
         td_error = chosen_action_qvals - targets.detach()
@@ -157,6 +177,16 @@ class WIQL(NIQLBasePolicy):
         loss = loss.sum() / mask.sum()
         self.model.tower_stats["loss"] = to_numpy(loss)
         tb_add_scalar(self, "loss", loss.item())
+
+        # vae loss
+        # uniform_batch = self.convert_batch_to_tensor(uniform_batch)
+        # vae_data = construct_vae_data(
+        #     uniform_batch[SampleBatch.OBS], uniform_batch[SampleBatch.PREV_ACTIONS], kwargs.get("n_actions")
+        # )
+        # vae_loss = self.compute_vae_loss(vae_data)
+        # lamda = 0.5
+        # loss = loss + lamda * vae_loss
+        # tb_add_scalar(self, "vae_loss", vae_loss.item())
 
         # gather td error for each unique target for analysis (matrix game case - discrete reward)
         # if self.config.get("env_name") in DEBUG_ENVS:

niql/utils.py
@@ -11,7 +11,7 @@ from ray.rllib.models.preprocessors import get_preprocessor
 from ray.rllib.policy.rnn_sequencing import chop_into_sequences
 from ray.rllib.utils.metrics.learner_info import LEARNER_STATS_KEY
 from ray.rllib.utils.torch_ops import convert_to_torch_tensor
-from sklearn.cluster import DBSCAN
+from sklearn.cluster import DBSCAN, HDBSCAN
 from sklearn.neighbors import KernelDensity
 
 from niql.torch_kde import TorchKernelDensity
@@ -219,11 +219,9 @@ def tb_add_scalars(policy, label, values_dict):
 def target_distribution_weighting(policy, targets, sim_threshold):
     targets_flat = targets.reshape(-1, 1)
     if random.random() < policy.tdw_schedule.value(policy.global_timestep):
-        lds_weights = get_target_dist_weights_sk(
+        lds_weights = get_target_dist_weights_torch(
             targets_flat, sim_threshold
         )
-        scaling = len(lds_weights) / (lds_weights.sum() + 1e-7)
-        lds_weights *= scaling
         lds_weights = convert_to_torch_tensor(lds_weights, policy.device).reshape(*targets.shape)
         # min_w = max(1e-2, lds_weights.min())
         # lds_weights = torch.clamp(torch.log(lds_weights), min_w, max=2 * min_w)
@@ -259,13 +257,14 @@ def normalize_zero_mean_unit_variance(X):
     return X_normalized
 
 
-def get_target_dist_weights_torch(train_data, eval_data, kernel, bandwidth) -> np.array:
-    train_data = normalize_zero_mean_unit_variance(train_data)
-    eval_data = normalize_zero_mean_unit_variance(eval_data)
-    kde = TorchKernelDensity(bandwidth, kernel)
-    kde.fit(train_data)
-    densities = kde.score_samples(eval_data)
+def get_target_dist_weights_torch(targets, kernel, bandwidth=1) -> np.array:
+    targets = standardize(targets)
+    kde = TorchKernelDensity(kernel=TorchKernelDensity.gaussian, bandwidth=5.)
+    kde.fit(targets)
+    densities = kde.score_samples(targets)
     weights = 1. / (densities + 1e-7)
+    scaling = len(weights) / weights.sum()
+    weights *= scaling
     weights /= (weights.max() + 1e-7)
     return weights
 
@@ -304,10 +303,13 @@ def get_target_dist_weights_cl(targets, *args, **kwargs) -> np.array:
     eff_num_per_label = [emp_label_dist[bin_idx] for bin_idx in bin_index_per_label]
     weights = [1. / (x + 1e-6) for x in eff_num_per_label]
     tdw_weights = np.array(weights).reshape(len(data), -1)
+    scaling = len(tdw_weights) / (tdw_weights.sum() + 1e-7)
+    tdw_weights *= scaling
+    tdw_weights /= (tdw_weights.max() + 1e-7)
     return tdw_weights
 
 
-def cluster_labels(data, min_samples_in_cluster=2, eps=.5, n_clusters=100):
+def cluster_labels(data, min_samples_in_cluster=2, eps=10., n_clusters=100):
     data = standardize(data)
     # n_clusters = min(n_clusters, len(np.unique(labels)))
     # clustering = KMeans(n_clusters=n_clusters, random_state=seed, n_init="auto").fit(labels.reshape(-1, 1))