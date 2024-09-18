8b827c54e5629958dabaa33d62e12004ad421bfe
Brighter <brighteragyemang@gmail.com>
2024-09-18 11:48:31 +1000

    evaluate gru on predatorprey

--- niql/envs/__init__.py
+++ niql/envs/__init__.py
@@ -16,7 +16,7 @@ DEBUG_ENVS = ["TwoStepsCoopMatrixGame", "OneStepCoopMatrixGame"]
 
 
 def get_active_env(**kwargs):
-    return make_smac_env(**kwargs)
+    return make_predator_prey_env(**kwargs)
 
 
 def make_mpe_simple_spread_env(**kwargs):
